f5db0bdfea30a34a76cdd6d72cb4803e2a3199ce
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-30 09:12:19 +0100

    Add project files.

---
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsDetermined1()
+        {
+            double a = 3;
+            bool RespAttesa = true;
+            bool Resp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(RespAttesa, Resp);
+        }
+
+        [TestMethod]
+        public void TestIsDetermined2()
+        {
+            double a = -3;
+            bool RespAttesa = true;
+            bool Resp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(RespAttesa, Resp);
+        }
+
+        [TestMethod]
+        public void TestIsDetermined3()
+        {
+            double a = 0;
+            bool RespAttesa = false;
+            bool Resp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(RespAttesa, Resp);
+        }
+    }
+}
---
+++ EquazioniLibrary/Equazioni.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public static class Equazioni
+    {
+        public static bool IsDetermined(double a)
+        {
+            bool risposta = true;
+            if (a == 0)
+            {
+                risposta = false;
+            }
+            return risposta;
+        }
+
+ 
+    }
+}
